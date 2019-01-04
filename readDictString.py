b8eb4e3826561a60f812068c7e9b3746eeae4a1c
Hasan Suhaimi <srip_arif@yahoo.com.my>
2019-01-04 03:29:29 +0000

    Update readDictString.py
    fix some bug

--- readDictString.py
+++ readDictString.py
@@ -1,21 +1,22 @@
 import yaml
 
-#open for reading
-dataset = open("test.txt",'r')
+# open for reading
+with open("Dataset(sep29-aug30).txt", 'r') as dataset:
 
-#split every string ended with '\n' into a list of a string ['*']
-line_list = dataset.read().split('\n')
+    # split every string ended with '\n' into a list of a string ['*']
+    line_list = dataset.read().split('\n')
 
+    #for the end of line
+    end = ''
 
-for line in line_list:
-
-    #convert stringed dict into dict "*" -> {*:**}
-    line_dict = yaml.load(line)
-    #accessing the key
-    print(line_dict['created_at'])
-
-    
-#close the text file
-dataset.close()
-
-
+    for line in line_list:
+        if line != end:
+            # convert stringed dict into dict "*" -> {*:**}
+            line_dict = yaml.load(line)
+            # accessing the key
+            print(line_dict['created_at'])
+        else:
+            break
+            
+    # close the text file
+    dataset.close()
